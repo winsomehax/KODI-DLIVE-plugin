2497a8ca704dd01839970318f81c16ead6050146
winsomehax <winsomehax@gmail.com>
2020-12-03 16:23:48 +0000

    Python PEP8 formatting - no functional changes

--- dlivequery.py
+++ dlivequery.py
@@ -1,97 +1,101 @@
 import graphql
 
+
 def dq_dlive_userid_from_displayname(display_name):
-	client = graphql.GraphQLClient('https://graphigo.prd.dlive.tv/')
+    client = graphql.GraphQLClient('https://graphigo.prd.dlive.tv/')
 
-	result = client.execute('''query { userByDisplayName ( displayname: "'''+display_name+'''") { username } }''')
+    result = client.execute(
+        '''query { userByDisplayName ( displayname: "'''+display_name+'''") { username } }''')
 
-	print("!!! ", result)
-	m=result["data"]["userByDisplayName"]
+    print("!!! ", result)
+    m = result["data"]["userByDisplayName"]
 
-	if m is None:
-		user_id=None
-	else:
-		user_id=m["username"]
+    if m is None:
+        user_id = None
+    else:
+        user_id = m["username"]
 
-	return (user_id)
+    return (user_id)
+
 
 def dq_get_user_info(uname):
-	client = graphql.GraphQLClient('https://graphigo.prd.dlive.tv/')
+    client = graphql.GraphQLClient('https://graphigo.prd.dlive.tv/')
 
-	if uname == "":
-		uname = "winsomehax"
+    if uname == "":
+        uname = "winsomehax"
 
-	result = client.execute(
-		''' query { user(username: "''' + uname + '''") { following { totalCount list { displayname username 
+    result = client.execute(
+            ''' query { user(username: "''' + uname + '''") { following { totalCount list { displayname username 
 		livestream { thumbnailUrl title } pastBroadcasts { list { title length thumbnailUrl playbackUrl } } } } } 
 		} ''')
 
-	if result["data"]["user"] is None:
-		result = None
+    if result["data"]["user"] is None:
+        result = None
 
-	return result
+    return result
 
 
 def dq_get_all_live_streams(after):
-	client = graphql.GraphQLClient('https://graphigo.prd.dlive.tv/')
+    client = graphql.GraphQLClient('https://graphigo.prd.dlive.tv/')
 
-	result = client.execute(
-		'''query { livestreams (input: {showNSFW: true, order: TRENDING, after: "'''+after+'''"}) { pageInfo { startCursor, endCursor, 
+    result = client.execute(
+            '''query { livestreams (input: {showNSFW: true, order: TRENDING, after: "'''+after+'''"}) { pageInfo { startCursor, endCursor, 
 		hasNextPage, hasPreviousPage} list { id, permlink, ageRestriction, thumbnailUrl, disableAlert, title, 
 		createdAt, totalReward, watchingCount, creator {username, displayname}, view } } }''')
 
-	return result
+    return result
 
 
 def all_live_streams():
-	result = []
+    result = []
 
-	after_cursor = "0"
-	next_page = True
+    after_cursor = "0"
+    next_page = True
 
-	while next_page:
-		r = dq_get_all_live_streams(after_cursor)
-		next_page= r["data"]["livestreams"]["pageInfo"]["hasNextPage"]
-		after_cursor = r["data"]["livestreams"]["pageInfo"]["endCursor"]
+    while next_page:
+        r = dq_get_all_live_streams(after_cursor)
+        next_page = r["data"]["livestreams"]["pageInfo"]["hasNextPage"]
+        after_cursor = r["data"]["livestreams"]["pageInfo"]["endCursor"]
 
-		for u in r["data"]["livestreams"]["list"]:
-			result.append((u["title"], 'https://live.prd.dlive.tv/hls/live/' + u["creator"]["username"] + '.m3u8',
-						   u["thumbnailUrl"], u["creator"]["displayname"]))
+        for u in r["data"]["livestreams"]["list"]:
+            result.append((u["title"], 'https://live.prd.dlive.tv/hls/live/' + u["creator"]["username"] + '.m3u8',
+                                       u["thumbnailUrl"], u["creator"]["displayname"]))
 
-	return result
+    return result
 
 
 def following_live_streams(uname):
 
-	r = dq_get_user_info(uname)
-	result = []
+    r = dq_get_user_info(uname)
+    result = []
 
-	for u in r["data"]["user"]["following"]["list"]:
-		if u["livestream"] is not None:
-			result.append(
-				(u["livestream"]["title"], 'https://live.prd.dlive.tv/hls/live/' + u["username"] + '.m3u8',
-				 u["livestream"]["thumbnailUrl"], u["displayname"]))
+    for u in r["data"]["user"]["following"]["list"]:
+        if u["livestream"] is not None:
+            result.append(
+                (u["livestream"]["title"], 'https://live.prd.dlive.tv/hls/live/' + u["username"] + '.m3u8',
+                 u["livestream"]["thumbnailUrl"], u["displayname"]))
 
-	return result
+    return result
 
 
 def following(uname):
-	r = dq_get_user_info(uname)
-	result = []
+    r = dq_get_user_info(uname)
+    result = []
 
-	for u in r["data"]["user"]["following"]["list"]:
-		result.append((u["displayname"], u["username"]))
+    for u in r["data"]["user"]["following"]["list"]:
+        result.append((u["displayname"], u["username"]))
 
-	return result
+    return result
 
 
 def replays(username, uname):
-	r = dq_get_user_info(uname)
-	result = []
+    r = dq_get_user_info(uname)
+    result = []
 
-	for u in r["data"]["user"]["following"]["list"]:
-		if username == u["username"]:
-			for s in u["pastBroadcasts"]["list"]:
-				result.append((s["title"], s["length"], s["thumbnailUrl"], s["playbackUrl"]))
+    for u in r["data"]["user"]["following"]["list"]:
+        if username == u["username"]:
+            for s in u["pastBroadcasts"]["list"]:
+                result.append((s["title"], s["length"],
+                               s["thumbnailUrl"], s["playbackUrl"]))
 
-	return result
+    return result
